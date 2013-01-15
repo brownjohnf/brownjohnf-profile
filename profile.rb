5741019f73b3162e7b5c6893d668d286780785c1
John F. Brown <jack@brownjohnf.com>
2013-01-15 22:13:04 +0000

    added download functions

--- profile.rb
+++ profile.rb
@@ -13,3 +13,20 @@ end
 get '/contact' do
   haml :contact
 end
+
+get '/download/pdf' do
+  file = File.join(settings.public_folder, 'JohnBrown-Resume.pdf')
+  send_file(file, :disposition => 'attachment', :file_name => File.basename(file))
+end
+
+get '/download/word' do
+  file = File.join(settings.public_folder, 'JohnBrown-Resume.docx')
+  send_file(file, :disposition => 'attachment', :file_name => File.basename(file))
+end
+
+=begin
+get '/download/libreoffice' do
+  file = File.join(settings.public_folder, 'JohnBrown-Resume.odf')
+  send_file(file, :disposition => 'attachment', :file_name => File.basename(file))
+end
+=end
