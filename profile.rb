ed50ec68b23b359a5dd9fe7ba20fb53b2769637f
John (Jack) Brown <jack@brownjohnf.com>
2015-11-01 21:16:37 -0800

    Upgrade ruby/gems; edit right rail

--- profile.rb
+++ profile.rb
@@ -19,14 +19,3 @@ get '/download/pdf' do
   send_file(file, :disposition => 'attachment', :file_name => File.basename(file))
 end
 
-get '/download/word' do
-  file = File.join(settings.public_folder, 'JohnBrown-Resume.docx')
-  send_file(file, :disposition => 'attachment', :file_name => File.basename(file))
-end
-
-=begin
-get '/download/libreoffice' do
-  file = File.join(settings.public_folder, 'JohnBrown-Resume.odf')
-  send_file(file, :disposition => 'attachment', :file_name => File.basename(file))
-end
-=end
